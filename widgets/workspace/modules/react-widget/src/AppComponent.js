bc64bc8c860859ba204e06a3030b2b354d1cf2fa
Gabriel Prates <gabsprates@gmail.com>
2023-09-12 13:57:17 +0100

    feat(widgets): Add react-widget

---
+++ widgets/workspace/modules/react-widget/src/AppComponent.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function AppComponent(props) {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+
+      <div>
+        <span className="tag">
+          {Liferay.Language.get("portlet-namespace")}:
+        </span>
+        <span className="value">{props.portletNamespace}</span>
+      </div>
+
+      <div>
+        <span className="tag">{Liferay.Language.get("context-path")}:</span>
+        <span className="value">{props.contextPath}</span>
+      </div>
+
+      <div>
+        <span className="tag">
+          {Liferay.Language.get("portlet-element-id")}:
+        </span>
+        <span className="value">{props.portletElementId}</span>
+      </div>
+
+      <div>
+        <span className="tag">{Liferay.Language.get("configuration")}:</span>
+        <span className="value pre">
+          {JSON.stringify(props.configuration, null, 2)}
+        </span>
+      </div>
+    </div>
+  );
+}
