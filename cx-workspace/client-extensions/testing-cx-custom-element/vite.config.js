8eca08cbf00b19a486c8ce0e336844d3925657a5
Gabriel Prates <gabsprates@gmail.com>
2023-10-05 13:51:40 +0100

    feat(cx-workspace): Add CX configuration

--- cx-workspace/client-extensions/testing-cx-custom-element/vite.config.js
+++ cx-workspace/client-extensions/testing-cx-custom-element/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Should setup, matching with the CX id and friendlyURLMapping
+  // So we can load the images correctly:
+  base: "/o/testing-cx-custom-element",
+
   plugins: [react()],
-})
+  build: {
+    // Necessary because Liferay generates `build` and `dist`
+    // directories and we want to avoid conflicts:
+    outDir: "./vite-build",
+
+    rollupOptions: {
+      // That's what make vite not include `react` and `react-dom`
+      // inside the final bundle, so we can load that as module:
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
