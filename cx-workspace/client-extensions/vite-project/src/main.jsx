5256d1107ab1af4c5be8c355ed2379c868544b7b
Gabriel Prates <gabsprates@gmail.com>
2023-10-17 13:58:10 +0100

    feat: Add customElement CX

---
+++ cx-workspace/client-extensions/vite-project/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+class WebComponent extends HTMLElement {
+  connectedCallback() {
+    ReactDOM.createRoot(this).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  }
+}
+
+const ELEMENT_ID = "vite-project";
+
+if (!customElements.get(ELEMENT_ID)) {
+  customElements.define(ELEMENT_ID, WebComponent);
+}
