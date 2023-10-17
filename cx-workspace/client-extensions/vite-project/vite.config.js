7c2184983a7ac81af26af004e170744fcbb4ab00
Gabriel Prates <gabsprates@gmail.com>
2023-10-17 20:59:01 +0100

    test: Add test to customElement CX

--- cx-workspace/client-extensions/vite-project/vite.config.js
+++ cx-workspace/client-extensions/vite-project/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./testSetup.js",
+  },
+
   base: "/o/vite-project",
   plugins: [react()],
   build: {
